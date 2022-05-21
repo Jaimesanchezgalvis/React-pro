78655b133d9dc40eddcb3beed2cbeb7cf1f8a7a0
jaimesanchezgalvisacct <jaime.sanchez@acctglobal>
2022-05-21 17:41:42 +0200

    commit save

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,49 +1,49 @@
+import { Suspense } from "react";
+import {
+  Routes,
+  Route,
+  NavLink,
+  Navigate,
+  BrowserRouter,
+} from "react-router-dom";
+import { routes } from "./routes";
 
-import { Suspense } from 'react';
-import { Routes, Route, NavLink, Navigate, BrowserRouter } from 'react-router-dom';
-import { routes } from './routes';
-
-import logo from '../logo.svg';
+import logo from "../logo.svg";
 
 interface Isactiveprop {
-    isActive: boolean;
+  isActive: boolean;
 }
 
 export const Navigation = () => {
-
-    return (
-        <Suspense fallback={null}>
-            <BrowserRouter>
-                <div className="main-layout">
-                    <nav>
-                        <img src={logo} alt="" />
-                        <ul>
-                            {
-                                routes.map(({ to, name }) => (
-                                    <li key={to}>
-                                        <NavLink
-                                            to={to}
-                                            className={({ isActive }: Isactiveprop) => isActive ? 'nav-active' : ''}>
-                                            {name}
-                                        </NavLink>
-                                    </li>
-                                ))}
-                        </ul>
-                    </nav>
-                    <Routes>
-                        {
-                            routes.map(({ path, Component }) => (
-                                <Route
-                                    key={path}
-                                    path={path}
-                                    element={<Component />}
-                                />
-                            ))
-                        }
-                        <Route path="/*" element={<Navigate to={routes[0].to} replace />} />
-                    </Routes>
-                </div>
-            </BrowserRouter>
-        </Suspense>
-    )
-}
+  return (
+    <Suspense fallback={null}>
+      <BrowserRouter>
+        <div className="main-layout">
+          <nav>
+            <img src={logo} alt="" />
+            <ul>
+              {routes.map(({ to, name }) => (
+                <li key={to}>
+                  <NavLink
+                    to={to}
+                    className={({ isActive }: Isactiveprop) =>
+                      isActive ? "nav-active" : ""
+                    }
+                  >
+                    {name}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          </nav>
+          <Routes>
+            {routes.map(({ path, Component }) => (
+              <Route key={path} path={path} element={<Component />} />
+            ))}
+            <Route path="/*" element={<Navigate to={routes[0].to} replace />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </Suspense>
+  );
+};
